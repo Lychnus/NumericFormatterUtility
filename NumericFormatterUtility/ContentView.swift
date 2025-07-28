0847a5b31e7287bb0fa2e50ed7ca6954a05f4afb
Tahir Anil Oghan <t.anil.oghan@gmail.com>
2025-07-28 17:15:44 +0300

    Initial Commit

---
+++ NumericFormatterUtility/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NumericFormatterUtility
+//
+//  Created by Tahir Anil Oghan on 28.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NumericFormatterUtility/NumericFormatterUtilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumericFormatterUtilityApp.swift
+//  NumericFormatterUtility
+//
+//  Created by Tahir Anil Oghan on 28.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct NumericFormatterUtilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
